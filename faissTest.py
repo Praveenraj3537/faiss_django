060dfc7e4dc281a25315474a31ad44a48c286bb5
Praveenraj Pattar <pattar.praveenraj3537@gmail.com>
2024-12-03 18:50:36 +0530

    changes

--- faissTest.py
+++ faissTest.py
@@ -3,7 +3,7 @@ import faiss
 d = 64                           # dimension
 nb = 100000                      # database size
 nq = 10000                       # nb of queries
-np.random.seed(1234)             # make reproducible
+# np.random.seed(1234)             # make reproducible
 xb = np.random.random((nb, d)).astype('float32')
 xb[:, 0] += np.arange(nb) / 1000.
 xq = np.random.random((nq, d)).astype('float32')
